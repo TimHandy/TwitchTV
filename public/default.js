7f4e921ff002984155d5757f108489ccf081e9b2
Tim Handy <tjhandy@gmail.com>
2016-08-15 16:39:34 +0100

    Add final MVP requirements. Code complete

--- public/default.js
+++ public/default.js
@@ -1,18 +1,16 @@
-// TODO: Story: I can see whether Free Code Camp is currently streaming on Twitch.tv.
+// Story: I can see whether Free Code Camp is currently streaming on Twitch.tv.
 
-// TODO: Story: I can click the status output and be sent directly to the Free Code Camp's Twitch.tv channel.
+// Story: I can click the status output and be sent directly to the Free Code Camp's Twitch.tv channel.
 
-// TODO: Story: if a Twitch user is currently streaming, I can see additional details about what they are streaming.
+// Story: if a Twitch user is currently streaming, I can see additional details about what they are streaming.
 
-// TODO: Story: I will see a placeholder notification if a streamer has closed their Twitch account (or the account never existed). You can verify this works by adding brunofin and comster404 to your array of Twitch streamers.
+// Story: I will see a placeholder notification if a streamer has closed their Twitch account (or the account never existed). You can verify this works by adding brunofin and comster404 to your array of Twitch streamers.
 
-
-// TODO: select to view All/Online/Offline somehow
 // TODO: search within each of All/Online/Offline somehow
 
 
 
-var streamerNames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var streamerNames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "numfin", "brunofin", "comster404" ];
 
 // function requestTwitchStreamers() {
 // 	var streamObjects = [];
@@ -33,16 +31,22 @@ function requestTwitchStream() {
 		var twitchApiUrl = 'https://api.twitch.tv/kraken/streams/' + streamer + '?callback=?';
 		$.getJSON(twitchApiUrl, function(data) {
 			console.log(data);
-			if (data.stream !== null) {
-				var text = (data.stream.channel.display_name + " is live");
+			if (data.status === 422) {
+				var text = (streamer + " has closed their Twitch account (or the account never existed)");
+				var li = document.createElement('li');
+				li.innerHTML = text;
+				li.className = "stream-offline";
+				document.getElementById("streams-list").appendChild(li);
+			} else if (data.stream !== null) {
+				var currentlyStreamingWhat = data.stream.game;
+				var text = (data.stream.channel.display_name + " is streaming " + currentlyStreamingWhat);
 				var link = '<a href="https://twitch.tv/' + streamer + '">' + text + '</a>';
 				console.log(link);
 				var li = document.createElement('li');
 				li.innerHTML = link;
 				li.className = "stream-live";
 				document.getElementById("streams-list").appendChild(li);
-				$('.stream-live').prependTo('#streams-list'); // TODO: sort by name
-
+				$('.stream-live').prependTo('#streams-list'); // TODO: sort by name)
 			} else {
 				var text = (streamer + " is offline");
 				var link = '<a href="https://twitch.tv/' + streamer + '">' + text + '</a>';
@@ -77,7 +81,7 @@ $(document).ready(function() {
 
  	requestTwitchStream(); // call api to pull all data for the streamerNames
 	//console.log(streamObjects);
-	
+
 	$('#all-button').click(function() {
 		$('li').removeClass('hidden');
 	});
@@ -93,31 +97,3 @@ $(document).ready(function() {
 	});
 
 });
-
-	// for (var i = 0; i < streamObjects.length; i++) {
-	// 	printStreamLi(streamObjects[i]);
-	// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Here's an example call to fetch Free Code Camp's Twitch channel data:
-
-// $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
-//   console.log(data);
-// });
-
-
-// Use these people:
-//
